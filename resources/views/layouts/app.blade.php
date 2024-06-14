fe951b552c9ab275430c49edb661325f47d20ee5
Asukulu Ndela <200056500@aston.ac.uk>
2024-06-14 14:08:21 +0100

    landing page and some changes etc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,15 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="{{ asset('js/app.js') }}"></script>
 </head>
+<style>
+
+    </style>
 <body>
     @include('layouts.navbar')
     <div class="hero">
         @yield('content')
     </div>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,25 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PlantController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\WishlistController;
 
-Route::get('/', [PlantController::class, 'index'])->name('home');
+//Route::get('/', function () {
+    //return view('landing');
+    //return view('home');
+//});
+
+// Route for landing page
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Routes for plants
+//Route::get('/', [PlantController::class, 'index'])->name('home');
 Route::get('/plants', [PlantController::class, 'index'])->name('plants.index');
 Route::get('/plants/{slug}', [PlantController::class, 'show'])->name('plants.show');
+
+// Routes for categories
 Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
 
 //Routes for the cart
